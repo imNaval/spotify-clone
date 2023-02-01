c2400584a1a9ca821ba603023ea8f57b60382ab9
imNaval <nehranvl2017@gmail.com>
2023-02-01 23:59:28 +0530

    create basic setup for spotify-clone web app

---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+  return (
+    <div className='login'>
+        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" alt="spotify-logo" />
+
+        <a href=''>LOGIN WITH SPOTIFY</a>
+    </div>
+  )
+}
+
+export default Login
